b45abc3fe7567638db66ea7816fd3ad1405db87e
beginnerxo <otengalex400@gmail.com>
2025-05-21 17:44:09 -0400

    alex's changes

---
+++ calculator.py
@@ -0,0 +1,84 @@
+'''INPUT : floating number
+    OUTPUT: floating number
+
+'''
+
+
+# add two numbers. 
+def add(x, y):
+    addition = x+y
+    return addition
+  
+  
+  
+#subtract two numbers
+def sub(x:float, y: float) -> float:
+   return x - y
+   
+   
+#multiply two numbers
+def mul(x,y):
+    return x*y
+ 
+ 
+#divide two numbers. Account for dividing with zeros also
+
+def div(x,y):
+    if (y == 0):
+        return ("error")
+    else:
+        return x/y
+   
+   
+   
+
+#main method for calculations
+
+def main():
+   
+    
+    
+    #Various operations in a list
+    operation  = [+ ,-,*, /]
+    
+    if user == +  
+    
+  
+    '''accept the values for the calculations. 3 values to be exact needed here
+        FIRST NUMBER, OPERATION, SECOND NUMBER
+        
+    '''
+    
+    
+    firstValue = input("Enter the first ")
+    input_operation  = input("enter the operation")
+    secondValue = input("enter the second ")
+
+    
+    # search for the input_operation in the list of operations 
+    
+    if input_operation in operation:
+        if input_operation == "+":
+            print(add(firstValue,secondValue))
+        if input..
+        if ..
+        
+    
+    
+    
+    else:
+        print("This is invalid") 
+    
+        #use if and else statement to print out the appropriate operations and print out the correct values 
+        
+    
+            
+            
+            
+    
+    
+#main loop
+    
+if __name__ ==  "__main__":
+    main()
+    
