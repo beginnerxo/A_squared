7389e939d791d83c0f3cca49ba695a3fa3302d56
amvll <alessandramavilla1@gmail.com>
2025-05-23 11:42:25 +0200

    sqrt and module function added

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@
 '''
 
 # add two numbers.
-
+import math
  
 def add(x: float, y: float) -> float:
     addition = x + y
@@ -24,9 +24,21 @@ def div(x,y):
         return ("error")
     else:
         return x / y
+    
+def mod(x, y):
+    if (y == 0):
+        return "error"
+    else:
+        return x % y
    
 def pow(x, y):
     return x ** y
+
+def sqrt(x):
+    if (x < 0):
+        return "error"
+    else:
+        return math.sqrt(x)
 
 
 #cancel function: if user wrong to type, you cancel the action
@@ -37,38 +49,55 @@ def pow(x, y):
 def main():
 
     #Various operations in a list
-    operation  = ["+","-","*","/", "**"]
+    operation  = ["+","-","*","/", "**", "%", "V"]
     
     '''accept the values for the calculations. 3 values to be exact needed here
-        FIRST NUMBER, OPERATION, SECOND NUMBER
-        
+        FIRST NUMBER, OPERATION, SECOND NUMBER  
     '''
+    while True:
+        try:
+            firstValue = float(input("Enter the first value: "))
+            input_operation = input("Enter the operation: ")
+            
+            if input_operation == "V" in operation:    
+                    print(math.sqrt(firstValue))
+                    go_or_not = input("Do you wish to continue/ input yes or no: ")
+                    go_or_not.lower 
 
-    try:
-        firstValue = float(input("Enter the first value: "))
-        input_operation = input("enter the operation: ")
-        secondValue = float(input("enter the second value: "))
+                    if go_or_not != "yes":
+                        break
+            else:
+                secondValue = float(input("Enter the second value: "))
 
-    
-    # search for the input_operation in the list of operations 
-    
-        if input_operation in operation:
-            if input_operation == "+":
-                print(add(firstValue,secondValue))
-            if input_operation == "-":
-                print(sub(firstValue, secondValue))
-            if input_operation == "*":
-                print(mul(firstValue, secondValue))
-            if input_operation == "/":
-                print(div(firstValue, secondValue))
-            if input_operation == "**":
-                print(pow(firstValue, secondValue))
-        else:
-            print("This is invalid") 
-    except ValueError:
-        print("it has to be a float number")
-        #use if and else statement to print out the appropriate operations and print out the correct values 
-         
+        # search for the input_operation in the list of operations 
+        
+                if input_operation in operation:
+                    if input_operation == "+":
+                        print(add(firstValue,secondValue))
+                    if input_operation == "-":
+                        print(sub(firstValue, secondValue))
+                    if input_operation == "*":
+                        print(mul(firstValue, secondValue))
+                    if input_operation == "/":
+                        print(div(firstValue, secondValue))
+                    if input_operation == "**":
+                        print(pow(firstValue, secondValue))
+                    if input_operation == "%":
+                        print(mod(firstValue, secondValue))
+                    
+                else:
+                    print("This is invalid") 
+
+                go_or_not = input("Do you wish to continue/ input yes or no: ")
+                go_or_not.lower 
+
+                if go_or_not != "yes":
+                    break
+
+        except ValueError:
+            print("Error")
+                #use if and else statement to print out the appropriate operations and print out the correct values 
+            
     
 #main loop
     
