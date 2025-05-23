422ee2ee5bc22c7308f420a551b70f2e9cb190e1
beginnerxo <otengalex400@gmail.com>
2025-05-23 02:33:18 -0400

    PREVIEW OF CALC, MAJORITY OF FUNCTIONS DONE

--- calcutorgui.py
+++ calcutorgui.py
@@ -1,59 +1,138 @@
 import kivy
+
+import calculator
+
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput 
 from kivy.uix.button import Button 
+from kivy.core.window import Window
+
+from kivy.lang import Builder
 
 
-class Gridlayout(GridLayout):
+#set window size 
+Window.size = (500,700)
+
+
+Builder.load_file('calc.kv')
+
+
+
+class MyLayout(Widget):
+    def clear(self):
+        self.ids.calc_input.text = "0"
+        
+    #button pressing function 
+    def button_press(self, button):
+        
+        #default textbox variable 
+        default = self.ids.calc_input.text
+        
+        #determine whether the default is 0
+        if default == "0":
+            self.ids.calc_input.text = ""
+            self.ids.calc_input.text = f'{button}'
+        else:
+            self.ids.calc_input.text = f'{default}{button}'
     
-    #initialise infinite keywords
-    def __init__(self, **kwargs):
-        
-        #call grid layout constructor 
-        
-        super(GridLayout, self).__init__(**kwargs)
-        
-        
-        #set columns
-        self.cols =2.
-        
-        #Add widgets
-        self.add_widget(Label(text ="First value "))
-        
-        # add input box
-        
-        self.first = TextInput(multiline = False)
-        self.add_widget(self.first)
-
-
-
-        #SECOND 
-        
-        self.add_widget(Label(text ="Operation: "))
-        
-        # add input box
-        
-        self.operation = TextInput(multiline = False)
-        self.add_widget(self.operation)
-
-
     
-        ## Second Value 
-        self.add_widget(Label(text ="Second Value: "))
-                
-                # add input box
-                
-        self.second = TextInput(multiline = False)
-        self.add_widget(self.second)
+    
+    #backspace
+    def backspace(self):
+        default = self.ids.calc_input.text
+        
+        default_list = list(str(default))        
+        new_value=""
+        
+        for i in range((len(default_list))-1):
+            new_value =new_value+ default_list[i]
+            
+        self.ids.calc_input.text = f'{new_value}'
+        
+        
+        
+    
+    def dot(self):
+        default = self.ids.calc_input.text
+        default = f"{default}."
+        self.ids.calc_input.text = f'{default}'
+    
+        
+               
+    #operation sign functions 
+    def math_sign(self,sign):
+        #default textbox variable 
+        default = self.ids.calc_input.text
+        
+        #adding the plus sign 
+        self.ids.calc_input.text = f'{default}{sign}'
+        
+    
+    
+    
+    
+    
+    
+        
+    def equals(self):
+         default = self.ids.calc_input.text
+         
+         #for_addition 
+         
+         if "+" in default:
+             num_list = default.split("+")
+             
+             answer = float(0)
+             for i in num_list:
+                answer = calculator.add(answer,float(i))
+             
+             #printing the answer in the text box
+             self.ids.calc_input.text = f'{answer}'
+            
+        #for subtraxion  
+         if "-" in default:
+             num_list = default.split("-")
+             
+             answer = float(num_list[0])
+             #print(answer)
+             
+             for i in range(len(num_list)):
+                answer = calculator.sub(answer,float(i+1)) 
+             self.ids.calc_input.text = f'{answer}'
+
+         if "X" in default:
+             num_list = default.split("-")
+             
+             answer = float(num_list[0])
+             #print(answer)
+             
+             for i in range(len(num_list)):
+                answer = calculator.mul(answer,float(i+1)) 
+             self.ids.calc_input.text = f'{answer}'
+             
+         if "/" in default:
+             num_list = default.split("-")
+             
+             answer = float(num_list[0])
+             #print(answer)
+             
+             for i in range(len(num_list)):
+                answer = calculator.div(answer,float(i+1)) 
+             self.ids.calc_input.text = f'{answer}'
+             
+            
+            
+        
+        
 
 
 
 
 class MyCalc(App):
     def build(self):
-        return GridLayout()
+        return MyLayout()
         
     
     
